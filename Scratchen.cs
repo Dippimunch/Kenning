3e16a2347f226d7202d488e5594806dd2319d2c5
Dippimunch <dippimunch@aim.com>
2017-08-14 13:15:09 -0400

    Add files via upload

---
+++ Scratchen.cs
@@ -0,0 +1,68 @@
+﻿using System;
+//namespace
+
+//class xMain
+
+	//map
+		//Tiles
+		//dimensions
+			//array
+		//Resources
+			//Bounty
+				//Water
+				//mineral
+				//fertile
+				//mobile?
+		//terrain
+			//features
+			//climate
+		//population
+
+	//entity
+		//location
+			//(0, 0)
+		//inputs
+			//sight
+			//memory
+				//range of time
+			//ennumeration
+				//counting of discreet events
+			//stability
+				//variation of stat 
+			//interaction
+				//relative force
+		//actions
+			//movement
+				//hex scale
+					//resolution
+				//schism
+					//arbitrary chance
+						//dire straits
+					//population
+		//traits
+			//health
+			//exploit fear
+				//
+		//language
+			//Word(SwadArray[w]
+				//need to make word class
+			//Swadesh
+			//orthography
+				//phonemes
+					//eventually IPA
+					//consonants ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","x","z"]
+					//vowels ["a","e","i","o","u"]
+			//grammar
+				//register
+			//ling drift
+				//borrowing
+				//powerful/central pops more influential
+	//time
+		//begin
+			//0
+		//end
+
+		//step
+		//weather
+		
+//error handling
---
+++ Swadesh.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Swadesh
+{
+	public class Phonotactics
+	//restrictions in a language on the permissible combinations of phonemes
+//syllable structure, consonant clusters, and vowel sequences by means of phonotactical constraints
+	{
+		// Declare the array of two elements:
+		string[][] phoneme = new string[2][];
+
+		// Initialize the elements:
+		phoneme[0] = new string[18] {"b", "c", "d", "f", "g", "h", "j", "k", "l",
+				"m", "n", "p", "q", "r", "s", "t", "v", "x"};
+		phoneme[1] = new string[5] {"a", "e", "i", "o", "u"};
+		//{
+		//	new string[] {
+		//phonemeArray[
+		public Phonotactics()
+		{
+			}
+
+	}
+}
